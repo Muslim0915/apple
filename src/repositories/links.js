dc8d0836835a29910dfba538c6b9fcb0747e55ad
Muslim0915 <muslimpro0915@gmail.com>
2024-03-12 20:02:24 +0500

    second commit

---
+++ src/repositories/links.js
@@ -0,0 +1,13 @@
+export default  [
+    {id: 1, name: 'Store', url: '/store'},
+    {id: 2, name: 'Mac', url: '/mac'},
+    {id: 3, name: 'Ipad', url: '/ipad'},
+    {id: 4, name: 'Iphone', url: '/iphone'},
+    {id: 5, name: 'Watch', url: '/watch'},
+    {id: 6, name: 'Vision', url: '/vision'},
+    {id: 7, name: 'AirPods', url: '/airpods'},
+    {id: 8, name: 'Tv & Home', url: '/tv-home'},
+    {id: 9, name: 'Entertainment', url: '/entertainment'},
+    {id: 10, name: 'Accessories', url: '/accessories'},
+    {id: 11, name: 'Support', url: '/support'},
+]
